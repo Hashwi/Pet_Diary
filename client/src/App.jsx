b04f72c9e8c13121e67f8aaca6ad5bbb4dc1ed35
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-05-22 15:35:04 +0200

    Initial React commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className="read-the-docs">
+        Pet Diary
+      </h1>
+    </>
+  )
+}
+
+export default App
